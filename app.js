5137c9f3b5c3aede2840e58627c3bd4414b9f70d
Tayyab <heyitstay011@gmail.com>
2021-05-03 12:14:39 +0530

    Changed Outut Resolution

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 const canvas = document.getElementById('canvas');
     uploader = document.getElementById('uploader');
+    generateBtn = document.getElementById('generate');
     context = canvas.getContext('2d');
     patternContainer = document.querySelector('.pattern-container');
+    message = document.querySelector('.message');
     reader = new FileReader(); 
     image = new Image();
 
@@ -26,53 +28,75 @@ const uploadImage = (e) =>{
            
            // Draw
            context.drawImage(image, 0, 0);
-           generatePattern()
+           
+           let msg = `<p>Input image: width = ${image.width}px, height = ${image.height}px</p>`
+           message.innerHTML = msg;
        }
 
     }
 
 }
 
-
-uploader.addEventListener('change', uploadImage);
-
+let previousSrc = null;
 
 const generatePattern = () => {
-        // Temporary Canvas to draw the output image 
-    let tempCanvas =  document.createElement('canvas');
-        tempContext = tempCanvas.getContext('2d');
-        tempCanvas.height = tempCanvas.width = 500; 
-
-
-        // MiniCanvas to store the print 
-    let miniCanvas = document.createElement('canvas');
-        miniContext = miniCanvas.getContext('2d');
     
-        miniCanvas.height = miniCanvas.width = 100;
-        
-        // Draw a single print
-        miniContext.drawImage(image, 0, 0, miniCanvas.width, miniCanvas.height)
+        if(!image.src){
+            let msg = `<p class="red" >No Images Selected!</p>`
+            message.innerHTML = msg;
+            return;
+        }
+        if(image.src === previousSrc){
+            let msg = `<p class="red">Select New Image</p>`
+            message.innerHTML = msg;
+            return;
+        }
+   
+
+   previousSrc = image.src;
+   
+
+   // Temporary Canvas to draw the output image 
+let tempCanvas =  document.createElement('canvas');
+    tempContext = tempCanvas.getContext('2d');
+    tempCanvas.height = tempCanvas.width = 1000; 
+
+
+    // MiniCanvas to store the print 
+let miniCanvas = document.createElement('canvas');
+    miniContext = miniCanvas.getContext('2d');
+
+    miniCanvas.height = miniCanvas.width = 200;
     
+    // Draw a single print
+    miniContext.drawImage(image, 0, 0, miniCanvas.width, miniCanvas.height)
 
-        // Create a pattern using the print
-    let pattern = context.createPattern(miniCanvas, 'repeat');
 
-        // Draw
-    tempContext.fillStyle = pattern;
-    tempContext.fillRect(0, 0 , tempCanvas.width, tempCanvas.height);
-    
-    // Convert the canvas to a DataUrl 
-    let dataUrl = tempCanvas.toDataURL('image/png');
+    // Create a pattern using the print
+let pattern = context.createPattern(miniCanvas, 'repeat');
 
-    //console.log(dataUrl);
-    addImage(dataUrl);
+    // Draw
+tempContext.fillStyle = pattern;
+tempContext.fillRect(0, 0 , tempCanvas.width, tempCanvas.height);
+
+// Convert the canvas to a DataUrl 
+let dataUrl = tempCanvas.toDataURL('image/png');
+
+//console.log(dataUrl);
+addImage(dataUrl);
 
 }
+
+
+uploader.addEventListener('change', uploadImage);
+generateBtn.addEventListener('click', generatePattern)
+
+
 
 const addImage = (dataUrl) => {
 
     let pattern = ` <figure class="pattern">
-                        <img src="${dataUrl}" alt="pattern" height="300" >
+                        <img src="${dataUrl}" alt="pattern" height="340" >
                         <figcaption> <a class="download-btn" href="${dataUrl}" download="pattern.png" >Download</a> </figcaption>
                     </figure>
                     ` 
